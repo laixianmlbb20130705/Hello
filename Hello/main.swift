a00c1557bc491e71dd0034f06b55abf2e1057c72
赖贤 <laixian@tusmdeMacBook-Pro-5.local>
2014-08-22 15:52:04 +0800

    Initial Commit

---
+++ Hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello
+//
+//  Created by 赖贤 on 14-8-22.
+//  Copyright (c) 2014年 www.appcode.com XXXX. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
